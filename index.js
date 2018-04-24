985ad1c96fe05ef97bd29f16977980cff69b099f
NAVDEEP SIDHU <sidhu44@gmail.com>
2018-04-24 06:08:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
   var arrX =[]
   for (var i=0; i < arrMusicians.length ; i++ )
     {
-      arrX.push(arrMusicians[i] + 'plays' + arrInstruments[i])
+      arrX.push(arrMusicians[i] + ' plays ' + arrInstruments[i])
     }
   return arrX
 }
